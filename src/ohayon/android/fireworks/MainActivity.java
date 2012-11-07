d54dd5d8c3d7f11a103678668e3c6ee0ac2a6d8f
Nikki Ohayon <Nikki Ohayon@10.45.116.206>
2012-11-06 20:28:27 -0500

    Draws points onTouch()

--- src/ohayon/android/fireworks/MainActivity.java
+++ src/ohayon/android/fireworks/MainActivity.java
@@ -1,15 +1,30 @@
 package ohayon.android.fireworks;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnTouchListener{
 
+	DrawPanel draw;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        Log.d("OnCreateMsg", "running onCreate...");
+        draw = new DrawPanel(this);
+        draw.setOnTouchListener(this);
+        setContentView(draw);
     }
 
     @Override
@@ -17,4 +32,30 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void alertMessage( View view ) {
+    	TextView textView = (TextView) this.findViewById(R.id.helloWorldText);
+    	Log.d("ButtonClick", "The start app button was clicked");    	
+    	textView.setText("App was started!");
+    }
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		draw.points.add(new Point((int)event.getX(), (int)event.getY()) );
+		draw.colors.add(getColor());
+		v.invalidate();
+		Log.d("OnTouch", "on touch was called");
+		return false;
+	} 
+
+	private int getColor() {
+		Random generator = new Random();
+		return Color.argb(255, generator.nextInt(256), generator.nextInt(256),
+							generator.nextInt(256));
+	}
+	
+/*
+ * canvas - draw on the screen 
+ * openGL
+ */
 }
