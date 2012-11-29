bbce6f22213165a60c6c6545c68507d96165a66e
Nikki Ohayon <Nikki Ohayon@NOhayon-PC>
2012-11-28 23:46:39 -0500

    Fixed his eyes

--- src/ohayon/android/fireworks/explosions/SmileyFaceExplosion.java
+++ src/ohayon/android/fireworks/explosions/SmileyFaceExplosion.java
@@ -1,11 +1,10 @@
 package ohayon.android.fireworks.explosions;
 
-import android.graphics.Color;
-import android.graphics.Point;
 import java.util.ArrayList;
-import java.util.Random;
 
 import ohayon.android.fireworks.Firework;
+import android.graphics.Color;
+import android.graphics.Point;
 
 public class SmileyFaceExplosion implements Explosion {
 
@@ -13,24 +12,23 @@ public class SmileyFaceExplosion implements Explosion {
 	public ArrayList<Firework> explode(Firework f) {
 		ArrayList<Firework> list = new ArrayList<Firework>();
 		Point p = f.getPosition();
-
+		double ttl = random.nextDouble() + 3;
 		// circle
-		Random generator = new Random();
 		for (int i = 0; i < 65; i++) {
-			list.add(new Firework(p.x, p.y, f.getV(), (double) generator
-					.nextInt(360), Color.YELLOW, generator.nextDouble() + 3, null, null));
+			list.add(new Firework(p.x, p.y, f.getV(), (double) random
+					.nextInt(360), Color.YELLOW, ttl, null, null));
 		}
 
 		// eyes
 		list.add(new Firework(p.x, p.y, f.getV()-30, f.getTheta() - 25,
-				Color.BLUE, f.getTtl(), null, null));
+				Color.BLUE, ttl, null, null));
 		list.add(new Firework(p.x, p.y, f.getV()-30, f.getTheta() + 25,
-				Color.BLUE, generator.nextDouble() + 3, null, null));
+				Color.BLUE, ttl, null, null));
 
 		// mouth
 		for (int i = 0; i < 65; i++) {
-			list.add(new Firework(p.x, p.y, f.getV()-30, -(double) generator
-					.nextInt(180), Color.RED, generator.nextDouble() + 3, null, null));
+			list.add(new Firework(p.x, p.y, f.getV()-30, -(double) random
+					.nextInt(180), Color.RED, ttl, null, null));
 		}
 		
 		return list;
